61b4fb89a6df3b1266edc4d0f89b6e6b2b49e9aa
Moises Herrera <moises.herrera@fdmgroup.com>
2020-05-17 14:25:31 -0700

    Create student class, initialize components file

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+import {Student} from './student';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['.student.component.css']
+})
+export class StudentComponent{
+
+}
