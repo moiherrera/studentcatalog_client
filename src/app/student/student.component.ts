6d6c8c219663c8d49a91ed81c59b2600638d2761
Moises Herrera <moises.herrera@fdmgroup.com>
2020-05-17 18:50:48 -0700

    Implemet Get Functionality

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,11 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {Student} from './student';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
-export class StudentComponent{
+export class StudentComponent implements OnInit{
+    students: Student[];
+    constructor(private _studentService: StudentService){}
 
+    ngOnInit(): void{
+      this.getStudents();
+    }
+
+    getStudents(): void{
+      this._studentService.getAllStudents().subscribe((studentData) => {this.students = studentData,console.log(studentData)}, (error) => {console.log(error);});
+    }
 }
--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -1,6 +1,6 @@
-import {Http, Response, RequestOptions, Headers} from '@angular/http';
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxja/Observable';
+import {Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable , of} from 'rxjs'
 import {Student} from './student';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -11,9 +11,7 @@ export class StudentService{
   constructor(private _httpService: Http){}
 
   getAllStudents(): Observable<Student[]>{
-    return this._httpService.get("http://localhost:8050/studentcatalog/api/student")
-    .map((response: Response) => response.json())
-    .catch(this.handleError);
+    return this._httpService.get("http://localhost:8050/studentcatalog/api/student").map((response: Response) => response.json()).catch(this.handleError);
   }
 
   private handleError(error: Response){
